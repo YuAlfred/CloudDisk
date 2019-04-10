6d509c7a9630b1151461dbf7e88b64dcb5b9ce0f
zhipeng.zhang <345849402@qq.com>
2019-04-10 17:14:19 +0800

    :construction: 数据库

---
+++ src/main/resources/SQL/CloudDisk.sql
@@ -0,0 +1,71 @@
+/*
+SQLyog 企业版 - MySQL GUI v8.14 
+MySQL - 5.7.22-log : Database - spring-boot-bpmn
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`spring-boot-bpmn` /*!40100 DEFAULT CHARACTER SET utf8 */;
+
+USE `spring-boot-bpmn`;
+
+/*Table structure for table `elfinder_file` */
+
+DROP TABLE IF EXISTS `elfinder_file`;
+
+CREATE TABLE `elfinder_file` (
+  `id` int(7) unsigned NOT NULL AUTO_INCREMENT,
+  `parent_id` int(7) unsigned NOT NULL,
+  `name` varchar(256) NOT NULL,
+  `content` longblob NOT NULL,
+  `size` int(10) unsigned NOT NULL DEFAULT '0',
+  `mtime` int(10) unsigned NOT NULL DEFAULT '0',
+  `mime` varchar(256) NOT NULL DEFAULT 'unknown',
+  `read` enum('1','0') NOT NULL DEFAULT '1',
+  `write` enum('1','0') NOT NULL DEFAULT '1',
+  `locked` enum('1','0') NOT NULL DEFAULT '0',
+  `hidden` enum('1','0') NOT NULL DEFAULT '0',
+  `width` int(5) NOT NULL DEFAULT '0',
+  `height` int(5) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `parent_name` (`parent_id`,`name`),
+  KEY `parent_id` (`parent_id`)
+) ENGINE=MyISAM AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
+
+/*Data for the table `elfinder_file` */
+
+insert  into `elfinder_file`(`id`,`parent_id`,`name`,`content`,`size`,`mtime`,`mime`,`read`,`write`,`locked`,`hidden`,`width`,`height`) values (1,0,'DATABASE','',0,0,'directory','1','1','0','0',0,0);
+
+/*Table structure for table `sys_user` */
+
+DROP TABLE IF EXISTS `sys_user`;
+
+CREATE TABLE `sys_user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
+  `role_code` varchar(255) NOT NULL,
+  `role_name` varchar(255) NOT NULL,
+  `gmt_create` datetime NOT NULL,
+  `gmt_update` datetime NOT NULL,
+  `nickname` varchar(255) DEFAULT NULL,
+  `user_create` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
+
+/*Data for the table `sys_user` */
+
+insert  into `sys_user`(`id`,`username`,`password`,`email`,`role_code`,`role_name`,`gmt_create`,`gmt_update`,`nickname`,`user_create`) values (1,'admin','23f90100463d27633c61a711a8bf8ea5072710952454d509','345849402@qq.com','admin','管理员','2019-03-21 14:30:57','2019-03-21 14:30:57','admin',1);
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
